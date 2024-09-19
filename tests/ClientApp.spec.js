5fddf289f411e09cd47c91ff470f3e9bbe364a23
szymon.boreczek88 <szymon.boreczek88@gmail.com>
2024-09-19 11:08:13 +0200

    ClientApp testing

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -2,15 +2,20 @@ const {test, expect} = require('@playwright/test');
 
 test('Client ecommerce Playwright test', async ({browser, page})=>
     {
+        const email = "gaming@gmail.com";
         const productName = 'IPHONE 13 PRO';
         const userName = page.locator('#userEmail');
         const passwordField = page.locator('#userPassword');
         const signIn = page.locator('#login'); 
         const products = page.locator('.card-body');
-        const creditcard = "2137 2115 2137 2115";
+        const creditcard = page.locator("input[type='text']").first();
+        const cvv = page.locator("input[type='text']").nth(1);
+        const cardName = page.locator("input[type='text']").nth(2);
+        const coupon = page.locator("input[type='text']").nth(3);
+        const country = page.locator("input[placeholder*='Country']");
         
         await page.goto('https://rahulshettyacademy.com/client');
-        await userName.fill("gaming@gmail.com");
+        await userName.fill(email);
         await passwordField.fill("KochamGaming1");
         await signIn.click();
         await page.waitForLoadState('networkidle');
@@ -27,15 +32,25 @@ test('Client ecommerce Playwright test', async ({browser, page})=>
                 break;
             }
         }
-        // IPHONE 13 PRO
+        // Personal information
         await page.locator("[routerlink*='cart']").click();
         await page.locator("div li").first().waitFor();
         const bool = await page.locator("h3:has-text('IPHONE 13 PRO')").isVisible();
         expect(bool).toBeTruthy();
 
         await page.locator("button[type='button']").last().click();
-        await page.locator("input.input.txt.text-validated").first.fill(creditcard);
-        await page.locator("input[placeholder*='Country']").pressSequentially('pol');
+        await creditcard.fill('2137 2115 2137 2115');
+        await page.getByRole('combobox').first().selectOption('11');
+        await page.getByRole('combobox').nth(1).selectOption('03');
+        await cvv.fill('666');
+        await cardName.fill('Krzysztof Ibisz');
+        await coupon.fill('rahulshettyacademy');
+        await page.locator("button[type='submit']").click();
+
+
+        // Shipping information
+        await country.click();
+        await country.pressSequentially('pol');
         const dropdown = await page.locator('.ta-results');
         await dropdown.waitFor("button");
         const optionsCount = await dropdown.locator("button").count();
@@ -48,5 +63,24 @@ test('Client ecommerce Playwright test', async ({browser, page})=>
                 break;
             }
         }
-        await page.pause();
+        await expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+        await page.locator(".action__submit").click();
+        await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+        const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+
+        await page.locator("td [routerlink*='myorders']").click();
+        await page.locator("tbody").waitFor();
+        const orderRow = await page.locator("tbody tr");
+        for(let i = 0; i < await orderRow.count(); ++i)
+        {
+            const rowOrderId = await orderRow.nth(i).locator("th").textContent(); 
+            if(orderId.includes(rowOrderId))
+                {
+                    await orderRow.nth(i).locator("button").first().click();
+                    break;
+                }
+        }
+    const orderIdDetails = await page.locator(".col-text").textContent();
+    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+    await page.pause();
     });
